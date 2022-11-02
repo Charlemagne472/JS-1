af5981984eb1ec6b96065954a1a4e2e24e6d112d
Charlemagne472 <mr.chugay00@gmail.com>
2022-11-02 20:51:03 +0200

    task5

---
+++ task5/script.js
@@ -0,0 +1,19 @@
+let fruits = ["апельсин", "банан", "груша"];
+
+document.write( fruits + "<br\/>");
+
+document.write( fruits.length + "<br\/>");
+
+fruits.push("яблоко","ананас");
+fruits.unshift("грейпфрут");
+
+document.write( fruits + "<br\/>");
+
+document.write( fruits.length + "<br\/>");
+
+fruits.pop();
+fruits.shift();
+
+document.write( fruits + "<br\/>");
+
+document.write( fruits.length + "<br\/>");
