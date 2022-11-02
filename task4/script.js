5151a3f65890266452e68e9f0b79d627bf25fc05
Charlemagne472 <mr.chugay00@gmail.com>
2022-11-02 20:47:01 +0200

    task4

---
+++ task4/script.js
@@ -0,0 +1,4 @@
+let country = ["Германия", "Великобритания", "Франция", "Италия", "Испания"];
+let population = ["83 млн.", "67 млн.", "64 млн.", "59 млн.", "47 млн."];
+
+document.write ("<br\/>" + country[0] + ` ` + population[0] + "<br\/>" + country[1] + ` ` + population[1] + "<br\/>" + country[2] + ` ` + population[2] + "<br\/>" + country[3] + ` ` + population[3] + "<br\/>" + country[4] + ` ` + population[4])
